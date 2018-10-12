f6fb84a1084decc3e91a4fb2cce9d685430f6406
amalk <amalk@192.168.102.55>
2018-10-12 17:55:41 +0200

    Ajout insert

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -28,8 +28,57 @@ INSERT INTO typeproduit (id, Type) VALUES ('1', 'Alimentaire');
 INSERT INTO typeproduit (id, Type) VALUES ('2', 'Vestimentaire');
 INSERT INTO typeproduit (id, Type) VALUES ('3', 'Fourniture scolaire');
 
-INSERT INTO produit (typeProduit, id, description, libelle, libelleCourt, prix, idTypeProduit, idNatureAliment) VALUES ('Aliment', 1,'Riz Basmati long grain de qualité supérieure\r\nRiz basmati long grain de qualité supérieure. Matière première importée d\Inde ou du Pakistan.', 'Riz Basmati le paquet 1kg ', 'Riz Basmati ', 0.8, 1, 2);
-INSERT INTO produit (typeProduit, id, description, libelle, libelleCourt, prix, idTypeProduit, idNatureAliment) VALUES ('Aliment', 3,'Riz Basmati long grain de qualité supérieure\r\nRiz basmati long grain de qualité supérieure. Matière première importée d Inde ou du Pakistan.', 'Riz Basmati le paquet 5kg ', 'Riz Basmati ', 2.5, 1, 2);
-INSERT INTO produit (typeProduit, id, description, libelle, libelleCourt, prix, idTypeProduit, idNatureAliment) VALUES ('Aliment', 4, 'Pâtes alimentaires de qualité supérieure\r\nSemoule de BLE dur de qualité supérieure.\r\nPeut contenir des traces d\œufs.','Pâtes Coquillettes le paquet de 1 kg', 'Pâtes 1kg ', 0.60, 1, 2);
-INSERT INTO produit (typeProduit, id, description, libelle, libelleCourt, prix, idTypeProduit, idNatureAliment) VALUES ('Aliment', 5, 'Pâtes alimentaires de qualité supérieure\r\nSemoule de BLE dur de qualité supérieure.\r\nPeut contenir des traces d\œufs.', 'Pâtes Coquillettes le paquet de 2 kg', 'Pâtes 2kg ', 1.2, 1, 2);
-INSERT INTO produit (typeProduit, id, description, libelle, libelleCourt, prix, idTypeProduit, idNatureAliment) VALUES ('Aliment', 6, 'Pâtes alimentaires de qualité supérieure\r\nSemoule de BLE dur de qualité supérieure.\r\nPeut contenir des traces d\œufs.', 'Pâtes Coquillettes le paquet de 5 kg', 'Pâtes 5kg ', 2, 1, 2)
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '1', 'Riz Basmati long grain de qualité supérieure\r\nRiz basmati long grain de qualité supérieure. Matière première importée d Inde ou du Pakistan.\r\nSemoule de BLE dur de qualité supérieure.', 'Riz Basmati le paquet 1kg ', 'Riz Basmati ', '0.8', NULL, NULL, '1', '2', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '2', 'Riz Basmati long grain de qualité supérieure\r\nRiz basmati long grain de qualité supérieure. Matière première importée d Inde ou du Pakistan.\r\nSemoule de BLE dur de qualité supérieure.', 'Riz Basmati le paquetRiz B 2kg ', 'Riz Basmati ', '1', NULL, NULL, '1', '2', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '3', 'Riz Basmati long grain de qualité supérieure\r\nRiz basmati long grain de qualité supérieure. Matière première importée d Inde ou du Pakistan.\r\nSemoule de BLE dur de qualité supérieure', 'Riz Basmati le paquet 5kg ', 'Riz Basmati ', '2.5', NULL, NULL, '1', '2', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`)
+VALUES ('', '4', 'Pâtes alimentaires de qualité supérieure\r\nSemoule de BLE dur de qualité supérieure.\r\nPeut contenir des traces d œufs.', 'Pâtes Coquillettes le paquet de 1 kg', 'Pâtes 1kg ', '0.60', NULL, NULL, '1', '2', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`)
+VALUES ('', '5', 'Pâtes alimentaires de qualité supérieure\r\nSemoule de BLE dur de qualité supérieure.\r\nPeut contenir des traces d œufs.', 'Pâtes Coquillettes le paquet de 2 kg', 'Pâtes 2kg ', '1.2', NULL, NULL, '1', '2', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`)
+VALUES ('', '6', 'Pâtes alimentaires de qualité supérieure\r\nSemoule de BLE dur de qualité supérieure.\r\nPeut contenir des traces d œufs.', 'Pâtes Coquillettes le paquet de 5 kg', 'Pâtes 5kg ', '2', NULL, NULL, '1', '2', NULL, NULL);
+ 
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '7', 'Ingrédients : 100% semoule de BLE dur de qualité supérieure', 'Pâtes Spaghetti Panzani\r\nle paquet de 1 kg', 'Spaghetti 1 kg', '1', NULL, NULL, '1', '2', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES('', '8', 'Ingrédients : 100% semoule de BLE dur de qualité supérieure', 'Pâtes Spaghetti Panzani\r\nle paquet de 3 kg', 'Spaghetti 3 kg', '1.5', NULL, NULL, '1', '2', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '9', 'Ingrédients : 100% semoule de BLE dur de qualité supérieure', 'Pâtes Spaghetti Panzani\r\nle paquet de 5 kg', 'Spaghetti 5 kg', '2.5', NULL, NULL, '1', '2', NULL, NULL);
+ 
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '10', 'Couscous moyens\r\nSemoule de BLE dur de qualité supérieure.', 'Couscous grain moyen paquet de 1 kg', 'Couscous 1 kg', '1.10', NULL, NULL, '1', '2', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '11', 'Couscous moyens\r\nSemoule de BLE dur de qualité supérieure.', 'Couscous grain moyen paquet de 3 kg', 'Couscous 3 kg', '2.50', NULL, NULL, '1', '2', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '12', 'Couscous moyens\r\nSemoule de BLE dur de qualité supérieure.', 'Couscous grain moyen paquet de 5 kg', 'Couscous 5 kg', '3.50', NULL, NULL, '1', '2', NULL, NULL);
+
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '13', 'Blé dur précuit.\r\nPeut contenir des traces de soja.', 'Blé délicat Carrefour\r\nla boîte de 1kg', 'Blé délicat 1kg', '1.34', NULL, NULL, '1', '2', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '14', 'Blé dur précuit.\r\nPeut contenir des traces de soja.', 'Blé délicat Carrefour\r\nla boîte de 2kg', 'Blé délicat 2kg', '2.1', NULL, NULL, '1', '2', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '15', 'Blé dur précuit.\r\nPeut contenir des traces de soja.', 'Blé délicat Carrefour\r\nla boîte de 5kg', 'Blé délicat 5kg', '3.2', NULL, NULL, '1', '2', NULL, NULL);
+
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '16', 'Lentillesss vertes sachet 1kg\r\n100% lentilles vertes', 'Lentilles vertes le paquet de 1kg', 'Blé délicat 1kg', '1.1', NULL, NULL, '1', '1', NULL, NULL); 
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '17', 'Lentilles vertes sachet 1kg\r\n100% lentilles vertes', 'Lentilles vertes le paquet de 2kg', 'Blé délicat 2kg', '2.1', NULL, NULL, '1', '1', NULL, NULL); 
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '18', 'Lentilles vertes sachet 1kg\r\n100% lentilles vertes', 'Lentilles vertes le paquet de 2kg', 'Blé délicat 5kg', '3.5', NULL, NULL, '1', '1', NULL, NULL); 
+
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '19', 'Lentilles corail\r\nPeut contenir des céréales contenant du gluten.', 'Lentilles corail la boîte de 1kg', 'Lentilles corail 1kg', '1.8', NULL, NULL, '1', '1', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '20', 'Lentilles corail\r\nPeut contenir des céréales contenant du gluten.', 'Lentilles corail la boîte de 2kg', 'Lentilles corail 2kg', '3', NULL, NULL, '1', '1', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`) 
+VALUES ('', '21', 'Lentilles corail\r\nPeut contenir des céréales contenant du gluten.', 'Lentilles corail la boîte de 5kg', 'Lentilles corail 5kg', '4.5', NULL, NULL, '1', '1', NULL, NULL);
+
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`)
+VALUES ('', '22', 'Pois cassés. Peut contenir des traces de gluten', 'Pois cassé 1kg', 'Pois cassé la boite de 1kg', '1.8', NULL, NULL, '1', '1', NULL, NULL);
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`)
+VALUES ('', '23', 'Pois cassés. Peut contenir des traces de gluten', 'Pois cassé 2kg', 'Pois cassé la boite de 2kg', '3.5', NULL, NULL, '1', '1', NULL, NULL); 
+INSERT INTO `produit` (`DTYPE`, `id`, `description`, `libelle`, `libelleCourt`, `prix`, `adulte`, `sexe`, `idTypeProduit`, `idNatureAliment`, `idNatureFourniture`, `idNatureVetement`)
+VALUES ('', '24', 'Pois cassés. Peut contenir des traces de gluten', 'Pois cassé 5kg', 'Pois cassé la boite de 5kg', '4.5', NULL, NULL, '1', '1', NULL, NULL); 
